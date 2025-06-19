71007888219465f9cb1ff651619dc2917cc95b12
your name <manhdd22411@st.uel.edu.vn>
2025-06-20 00:36:27 +0700

    point

--- app/src/main/java/com/example/bepnhataapp/features/point/PointActivity.java
+++ app/src/main/java/com/example/bepnhataapp/features/point/PointActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import com.example.bepnhataapp.R;
 
 public class PointActivity extends AppCompatActivity {
@@ -31,6 +32,17 @@ public class PointActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(PointActivity.this, PointHistoryActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        AppCompatButton btnExchange = findViewById(R.id.btnExchange);
+        if (btnExchange != null) {
+            btnExchange.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(PointActivity.this, com.example.bepnhataapp.features.voucher.VoucherActivity.class);
                     startActivity(intent);
                 }
             });
